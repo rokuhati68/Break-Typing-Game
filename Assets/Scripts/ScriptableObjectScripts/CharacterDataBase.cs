bc8450f4f444362e34aa593a05c96dc92537d9cc
rokuhati68 <junpei200268@icloud.com>
2025-09-30 15:08:40 +0900

    Build

--- Assets/Scripts/ScriptableObjectScripts/CharacterDataBase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CharacterDataBase", menuName = "Scriptable Objects/CharacterDataBase")]
-public class CharacterDataBase : ScriptableObject
-{
-    public List<CharacterData> characterDataList;
-}
